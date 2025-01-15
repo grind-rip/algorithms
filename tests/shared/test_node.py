665186fc0996e4e8981bd74403c8edeb50deb2d6
nickolashkraus <0x@nickolaskraus.io>
2025-01-15 07:10:29 -0600

    Simplify unit test

--- tests/shared/test_node.py
+++ tests/shared/test_node.py
@@ -1,9 +1,6 @@
-from unittest import TestCase
-
 from src.shared.node import Node
 
 
-class TestNode(TestCase):
-    def test(self):
-        n = Node(0)
-        assert isinstance(n, Node)
+def test_node():
+    n = Node(0)
+    assert isinstance(n, Node)
