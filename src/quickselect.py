5e5fcee2d9a149fa7c5feaaa60737e29aba9cb58
nickolashkraus <0x@nickolaskraus.io>
2025-02-03 13:49:30 -0600

    Update 'Quickselect'

--- src/quickselect.py
+++ src/quickselect.py
@@ -12,6 +12,14 @@ less than or greater than the pivot. However, instead of recursing into both
 sides, as in quicksort, quickselect only recurses into one side-the side with
 the element it is searching for. This reduces the average complexity from
 O(nlog n) to O(n), with a worst case of O(n^2).
+
+Why is quickselect O(n)?
+------------------------
+Since quickselect only cares about the partition which contains the element we
+are looking for, half the elements are eliminated in each recursive call. This
+forms a geometric series which sums to 2*n (or just n):
+
+  n + n/2 + n/4 + ... ≈ 2n = O(n)
 """
 
 from src.shared.partition import partition
@@ -19,18 +27,19 @@ from src.shared.partition import partition
 
 def quickselect(l: list[int], left: int, right: int, k: int) -> int:
     """
-    Return the k-th element in the given list.
+    Return the kth element (0-based) in the given list.
     """
     if left == right:
         return l[left]
 
-    # Retrieve the index of the pivot by paritioning the list into elements
+    # Retrieve the index of the pivot by partitioning the list into elements
     # less than and greater than or equal to the pivot.
     pivot = partition(l, left, right)
 
-    # If k is equal to 'pivot', then 'pivot' is the k-th element in the list.
-    # Otherwise, execute quickselect on the parition comprising elments less
-    # then or greater than or equal to k.
+    # If k is equal to 'pivot', then l[pivot] is the kth element in the list.
+    # Otherwise, execute quickselect on the partition comprising elements less
+    # than or greater than or equal to the pivot. This partition is guaranteed
+    # to contain the kth element.
     if k == pivot:
         return l[k]
     elif k < pivot:
